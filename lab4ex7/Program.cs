299334b37161fa52bba9caca8245896d9faf03c5
Alex Filip <alexfilippaul@gmail.com>
2023-01-10 00:59:15 +0200

    Add project files.

---
+++ lab4ex7/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace lab4ex7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Scrieti o functie recursiva care pentru un numar “n” primit ca parametru, va afisa urmatoarea piramida a numerelor:
+            Console.WriteLine("Introduceti n: ");
+            int n = int.Parse(Console.ReadLine());
+            Pyramid(n);
+        }
+        static void Pyramid(int n)
+        {
+            if(n<=0)
+            {
+                return;
+            }
+            int i= n;
+            while(i!=0)
+            {
+                Console.Write(n + " ");
+                i--;
+            }
+            Console.WriteLine();
+            Pyramid(n - 1);
+        }
+    }
+}
